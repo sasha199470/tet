7714499293780575b7aa3b70bf96aa18a8d94cb1
Artur Belogur <artur.belogur.korbit@gmail.com>
2018-04-09 01:55:16 +0200

    Add validation

--- src/app/components/form-component/form-component.component.ts
+++ src/app/components/form-component/form-component.component.ts
@@ -2,7 +2,9 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {User} from '../../models/User';
 import {Observable} from 'rxjs/Observable';
 import { ErrorStateMatcher } from '@angular/material';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators} from '@angular/forms';
+import { isUndefined } from 'util';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-form-component',
@@ -10,6 +12,7 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./form-component.component.css']
 })
 export class FormComponentComponent implements OnInit {
+
   optionDoc = [
     'Паспорт',
     'Загран паспорт',
@@ -22,18 +25,47 @@ export class FormComponentComponent implements OnInit {
   
   @Output() change = new EventEmitter;
 
-  onlyRusTextFormControl = new FormControl({disabled: false}, [
+  onlyRusTextFormControl = new FormControl('', [
     Validators.required,
     Validators.pattern(/^[\u0400-\u04FF]*$/),
   ]);
 
-  formControls = new Map<string, FormControl>();
-  
+  onlyNumberFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^\d+$/),
+  ]);
+
+  dateFormControl = new FormControl('', [
+    Validators.required,
+    this.dateMinValidator
+  ]);
+
   constructor() { }
 
   ngOnInit() {}
   
   copyField(key) {
     this.change.emit(key);
+  }
+
+  dateMinValidator(control: FormControl) {
+    const date = moment(control.value, 'DD.MM.YYYY');
+
+    if (!date.isValid()) {
+      return {
+        dateInvalid: {
+          data: control.value
+        }
+      } 
+    }
+    if (date < moment('01.01.1991', 'DD.MM.YYYY')) {
+      return {
+        dateMin: {
+          date: date
+        }
+      }
+    }
+
+    return null;
   }
 }
