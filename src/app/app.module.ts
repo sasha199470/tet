84c8febf13dafc78ef4d62c6598bb2393eb54204
Artur Belogur <artur.belogur.korbit@gmail.com>
2018-04-08 23:11:47 +0200

    Valid surname

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,17 @@ import {NgxMaskModule} from 'ngx-mask';
 import { AppComponent } from './components/root-component/app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { FormComponentComponent } from './components/form-component/form-component.component';
-import {MatFormFieldModule, MatInputModule, MatSelectModule} from '@angular/material';
-import {FormsModule} from '@angular/forms';
+import {MatFormFieldModule, MatInputModule, MatSelectModule, ErrorStateMatcher, ShowOnDirtyErrorStateMatcher} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {UserService} from './services/user.service';
-
+import {DisableControlDirective} from './disable-control.directive'
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponentComponent
+    FormComponentComponent,
+    DisableControlDirective  
   ],
   imports: [
     BrowserModule,
@@ -24,9 +25,13 @@ import {UserService} from './services/user.service';
     MatInputModule,
     MatSelectModule,
     FormsModule,
-    NgxMaskModule.forRoot()
+    NgxMaskModule.forRoot(),
+    ReactiveFormsModule    
   ],
-  providers: [UserService],
+  providers: [
+    UserService,
+    {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/form-component/form-component.component.ts
+++ src/app/components/form-component/form-component.component.ts
@@ -1,6 +1,8 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {User} from '../../models/User';
 import {Observable} from 'rxjs/Observable';
+import { ErrorStateMatcher } from '@angular/material';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-component',
@@ -13,14 +15,24 @@ export class FormComponentComponent implements OnInit {
     'Загран паспорт',
     'Паспорт моряка'
   ];
+  
   @Input() user: User;
   @Input() type;
   @Input() copy;
+  
   @Output() change = new EventEmitter;
-  constructor(
-  ) { }
+
+  onlyRusTextFormControl = new FormControl({disabled: false}, [
+    Validators.required,
+    Validators.pattern(/^[\u0400-\u04FF]*$/),
+  ]);
+
+  formControls = new Map<string, FormControl>();
+  
+  constructor() { }
 
   ngOnInit() {}
+  
   copyField(key) {
     this.change.emit(key);
   }
